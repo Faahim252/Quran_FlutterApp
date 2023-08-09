dc13f6bc6c09ce60df116aa13a06f79d75522971
sayidka <sayidalia081@gmail.com>
2023-08-09 19:38:51 +0300

    on process

---
+++ lib/pages/splash.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quran/globals.dart';
+import 'package:quran/pages/Home.dart';
+
+class SplashScreanpage extends StatelessWidget {
+  const SplashScreanpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: background,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Quran App',
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Learn Quran and\nRecite Twice everyday',
+                  style: GoogleFonts.poppins(fontSize: 18, color: text),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      height: 450,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xFF672CBC),
+                      ),
+                      child: SvgPicture.asset('assets/svgs/splash.svg'),
+                    ),
+                    Positioned(
+                        left: 0,
+                        bottom: -23,
+                        right: 0,
+                        child: Center(
+                          child: GestureDetector(
+                            behavior: HitTestBehavior.opaque,
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => HomeScreen(),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 40, vertical: 16),
+                              decoration: BoxDecoration(
+                                color: orange,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Text(
+                                'Get Started',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
